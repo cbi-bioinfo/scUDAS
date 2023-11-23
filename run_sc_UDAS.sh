865043752236ee3949229af4366ee66a591cd65b
Chaelin Park <pcr0827@sookmyung.ac.kr>
2023-11-23 12:55:02 +0900

    delete a file

--- run_sc_UDAS.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source_X="./data/source_X.csv"
-source_Y="./data/source_Y.csv"
-target_X="./data/target_X.csv"
-target_Y="./data/target_Y.csv"
-
-python scUDAS.py $source_X $source_Y $target_X $target_Y 
